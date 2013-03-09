d77282fa7c117e506b0c2b98cc6c60ea04628a3e
anthonylynch90 <anthony-lynch@hotmail.com>
2013-03-09 09:57:16 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockAndShares::Application.config.secret_token = '6003e5652acc599a93b92d99102f1dcdbccb25ff8e17208c7b5b8028ce1a6355470df15dce1b94a862a5d0c9d1c20ca0af81d51fa3699c6edc984e66289a15ba'
