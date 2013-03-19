1ba089c965fb4b9b697b145d85c8241dfa5030c3
anthonylynch90 <anthony-lynch@hotmail.com>
2013-03-19 11:27:08 +0000

    Added user authentication and password digest. Created rspec integration tests.

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130310151544) do
+ActiveRecord::Schema.define(:version => 20130319102922) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@
 #
 #  id         :integer         not null, primary key
 #  name       :string(255)
+
 #  email      :string(255)
 #  created_at :datetime        not null
 #  updated_at :datetime        not null
@@ -12,12 +13,15 @@
 require 'spec_helper'
 
 describe User do
-  before {@user = User.new(name: "Example User", email: "user@example.com")}
+  before {@user = User.new(name: "Example User", email: "user@example.com", password: "abcdef", password_confirmation: "abcdef")}
   
   subject{@user}
   
   it{should respond_to(:name)}
-  it{should respond_to(:email)}  
+  it{should respond_to(:email)} 
+  it{should respond_to(:password_digest)} 
+  it{should respond_to(:password)} 
+  it{should respond_to(:password_confirmation)} 
   
   describe "When name is not present" do
 	before{@user.name = ""}
@@ -25,17 +29,17 @@ describe User do
   end
   
   describe "When name is not present" do
-	before{@user.update_attributes(name: "example", email: "")}
+	before{@user.update_attributes(name: "example", email: "", password: "abcdef", password_confirmation: "abcdef")}
 	it{should_not be_valid}
   end
   
   describe "When name is too long" do
-	before{@user.update_attributes(name: "a"*51, email: "user@example.com")}
+	before{@user.update_attributes(name: "a"*51, email: "user@example.com", password: "abcdef", password_confirmation: "abcdef")}
 	it{should_not be_valid}
   end
   
   describe "Checks name of 50 is valid" do
-	before{@user.update_attributes(name: "a"*50, email: "user@example.com")}
+	before{@user.update_attributes(name: "a"*50, email: "user@example.com", password: "abcdef", password_confirmation: "abcdef")}
 	it{should be_valid}
   end
   
@@ -62,5 +66,36 @@ describe User do
 		user_with_same_email.save
 	end
 	it{should_not be_valid}
+  end
+  
+   describe "When password is not present" do
+	before {@user.password = @user.password_confirmation = " "}
+	it{should_not be_valid}
+  end
+  
+  describe "When password doesnt match confirmation" do
+	before {@user.password_confirmation = "mismatch"}
+	it{should_not be_valid}
+  end
+  
+  describe "When password confirmation is nil" do
+	before {@user.password = @user.password_confirmation = nil}
+	it{should_not be_valid}
+  end
+  
+  describe "return value of authenticate method" do
+  
+	before do
+		@user = User.new(name: "Example User", email: "user@example.com", password: "abcdef", password_confirmation: "abcdef")
+		@user.save
+	end
+	
+	describe "valid password" do
+		it{should == @user.authenticate(@user.password)}
+	end
+	
+	describe "invalid password" do
+		it{should_not == @user.authenticate("Invalid")}
+	end
   end
 end
